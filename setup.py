c8a5c706f116c3a2a5127363ff14e3d12887a98f
Adam Michaleski <adam@prairieapps.com>
2020-01-07 21:09:30 -0600

    Pypi configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'py-schluter',
+  packages = ['schluter'],
+  version = '0.1.0',
+  license='MIT',
+  description = 'Python API for Schluter DITRA-HEAT thermostat',
+  author = 'Adam Michaleski',
+  author_email = 'adam@prairieapps.com',
+  url = 'https://github.com/prairieapps/py-schluter',
+  requires=['requests']
+)
