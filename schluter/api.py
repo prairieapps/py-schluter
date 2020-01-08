e966f05b249520d2a5417601a6a2b75dc9d9c0a6
Adam Michaleski <adam@prairieapps.com>
2020-01-07 20:50:31 -0600

    Initial commit

---
+++ schluter/api.py
@@ -0,0 +1,55 @@
+import logging
+import json
+from requests import request, Session
+
+API_BASE_URL = "https://ditra-heat-e-wifi.schluter.com"
+API_AUTH_URL = API_BASE_URL + "/api/authenticate/user"
+API_GET_THERMOSTATS_URL = API_BASE_URL + "/api/thermostats"
+API_APPLICATION_ID = 7
+
+_LOGGER = logging.getLogger(__name__)
+
+class Api:
+    def __init__(self, timeout=10, command_timeout=60, http_session: Session = None):
+        self._timeout = timeout
+        self._command_timeout = command_timeout
+        self._http_session = http_session
+
+    def get_session(self, email, password):
+        response = self._call_api(
+            "post", 
+            API_AUTH_URL, 
+            json = { 
+                'Email': email, 
+                'Password': password, 
+                'Application': API_APPLICATION_ID
+            }).json()
+
+        return response
+    
+    def get_thermostats(self, sessionId):
+        thermostats = self._call_api( "get", API_GET_THERMOSTATS_URL, sessionId).json()
+        groups = thermostats["Groups"]
+
+        thermostat_list = []
+        for group in groups:
+            thermostat_list.append(group["Thermostats"])
+
+        return json.dumps(thermostat_list)
+
+    def _call_api(self, method, url, sessionId = None, **kwargs):
+        payload = kwargs.get("params") or kwargs.get("json")
+
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = self._timeout
+        
+        _LOGGER.debug("Calling %s with payload=%s", url, payload)
+
+        response = self._http_session.request(method, url, **kwargs) if\
+            self._http_session is not None else\
+            request(method, url, params = { 'sessionId': sessionId }, **kwargs)
+
+        _LOGGER.debug("API Response received: %s - %s", response.status_code, response.content)
+
+        response.raise_for_status()
+        return response
---
+++ schluter/thermostat.py
@@ -0,0 +1,67 @@
+class Thermostat():
+    def __init__(self, data):
+        self._serial_number = data["SerialNumber"]
+        self._name = data["Room"]
+        self._group_id = data["GroupId"]
+        self._group_name = data["GroupName"]
+        self._temperature = data["Temperature"]
+        self._set_point_temp = data["SetPointTemp"]
+        self._manual_temp = data["ManualTemperature"]
+        self._is_online = data["Online"]
+        self._is_heating = data["Heating"]
+        self._max_temp = data["MaxTemp"]
+        self._min_temp = data["MinTemp"]
+        self._kwh_charge = data["KwhCharge"]
+        self._load_measured_watt = data["LoadMeasuredWatt"]
+    
+    @property
+    def serial_number(self):
+        return self._serial_number
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def group_id(self):
+        return self._group_id
+    
+    @property
+    def group_name(self):
+        return self._group_name
+    
+    @property
+    def temperature(self):
+        return self._temperature
+
+    @property
+    def set_point_temp(self):
+        return self._set_point_temp
+    
+    @property
+    def manual_temp(self):
+        return self._manual_temp
+    
+    @property
+    def is_online(self):
+        return self._is_online
+    
+    @property
+    def is_heating(self):
+        return self._is_heating
+    
+    @property
+    def max_temp(self):
+        return self._max_temp
+    
+    @property
+    def min_temp(self):
+        return self._min_temp
+    
+    @property
+    def kwh_charge(self):
+        return self._kwh_charge
+
+    @property
+    def load_measured_watt(self):
+        return self._load_measured_watt
