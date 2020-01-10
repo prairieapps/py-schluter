9c106c4eaadbeb5235fcb30995bbaf4729381fdf
Adam Michaleski <adam@prairieapps.com>
2020-01-09 23:59:03 -0600

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'py-schluter',
   packages = ['schluter'],
-  version = '0.1.2',
+  version = '0.1.3',
   license='MIT',
   description = 'Python API for Schluter DITRA-HEAT thermostat',
   author = 'Adam Michaleski',
